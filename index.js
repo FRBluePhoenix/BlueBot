e1cf667b4869d83d1f6b3b853106b9679b0dd5b8
FRBluePhoenix <66485837+FRBluePhoenix@users.noreply.github.com>
2020-06-05 18:26:15 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,44 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const config = require('./config.json')
+const fs = require("fs")
+
+
+client.commands = new Discord.Collection()
+let prefix = config.prefix;
+
+var nombreAleatoire = Math.round(Math.random()*2);
+
+ 
+client.login(config.token);
+ 
+client.on("ready", () => {
+    console.log("connecté !")
+    client.user.setStatus("online")
+    client.user.setActivity("Être developper",{type: 1})
+});
+
+//division des commandes
+const loadCommands = (dir = "./commands/") => {
+    fs.readdirSync(dir).forEach(dirs => {
+      const commands = fs.readdirSync(`${dir}/${dirs}/`).filter(files => files.endsWith(".js"));
+  
+      for (const file of commands) {
+        const getFileName = require(`${dir}/${dirs}/${file}`);
+        client.commands.set(getFileName.help.name, getFileName);
+        console.log(`Commande chargée: ${getFileName.help.name}`);
+      };
+    });
+  };
+  
+  loadCommands();
+
+//Renvoie Messages
+client.on('message', message => {
+    if (!message.content.startsWith(PREFIX) || message.author.bot) return;
+    const args = message.content.slice(PREFIX.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+    
+    if (!client.commands.has(command)) return;
+    client.commands.get(command).run(client, message, args);
+  });
