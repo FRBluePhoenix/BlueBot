61303ef443ca140c51de30f4027bf5ea022e662d
FRBluePhoenix <66485837+FRBluePhoenix@users.noreply.github.com>
2020-06-06 18:06:22 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const config = require('./config.json')
-const fs = require("fs")
+const fs = require("fs");
+const config = require("./config.js");
 
 
 client.commands = new Discord.Collection()
-let prefix = config.prefix;
+let PREFIX = config.prefix
 
-
-
+var nombreAleatoire = Math.round(Math.random()*2);
  
 client.login(config.token);
  
@@ -25,7 +24,7 @@ const loadCommands = (dir = "./commands/") => {
   
       for (const file of commands) {
         const getFileName = require(`${dir}/${dirs}/${file}`);
-        client.commands.set(getFileName.help.name, getFileName);
+        client.commands.set(getFileName.help, getFileName);
         console.log(`Commande chargée: ${getFileName.help.name}`);
       };
     });
@@ -42,3 +41,35 @@ client.on('message', message => {
     if (!client.commands.has(command)) return;
     client.commands.get(command).run(client, message, args);
   });
+
+
+/* if(message.content === prefix + "ping") { 
+        message.channel.send("Pong")
+    }
+
+    if (message.content === prefix + "Quelle est ton jeu favori") {  //commande jeu favori
+        const replies = ["Dur de choisir parmis tous les jeux disponibles mais je dirai GTA", "Dur de choisir mais je dirai Monster Hunter","Selon moi le meilleur jeu est Ride 3"]
+        message.replytext = Math.floor((Math.random() * replies.length) + 0);
+        message.channel.send(replies[message.replytext]
+        );
+    }
+    
+    if(message.content === prefix + "" )  {
+        if(message.author.bot === false) {
+            message.channel.send("Je ne suis pas sur de comprendre, êtes-vous sure de la commande?",)
+        }
+    }
+
+
+
+    if(message.content === prefix + "Deconnection") { //commande deconnexion
+        message.channel.send("**Déconnection en cours**")
+            function fonctionAEtape(etape) {
+                setTimeout(function() { 
+                    message.channel.send("**Déconnécter**")
+                    client.user.setStatus("offline"),
+                5000, 2)};
+            }
+        }
+    }
+*/
